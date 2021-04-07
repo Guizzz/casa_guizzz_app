24cf1b79d92e357c7690ba0b368c4d627d093b79
Guizzz <45075994+Guizzz@users.noreply.github.com>
2021-04-08 00:25:24 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/guizzz/casa_guizzz_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.guizzz.casa_guizzz_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Casa Guizzz app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Casa Guizzz App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String risposta= "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            InkWell(
+              child: Container(
+                color: Colors.blue,
+                width: MediaQuery.of(context).size.width/3,
+                height: MediaQuery.of(context).size.height/8,
+              ),
+              onTap: blink,
+            ),
+            InkWell(
+              child: Container(
+                color: Colors.red,
+                width: MediaQuery.of(context).size.width/3,
+                height: MediaQuery.of(context).size.height/8,
+              ),
+              onTap: endBlink,
+            ),
+            Text(risposta),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future<bool> blink() async
+  {
+    final url = Uri.http("192.168.1.120:8889","/blink");
+    final response = await http.get(url);
+    if (response.statusCode == 200)
+    {
+      setState(() {
+        risposta=response.body;
+      });
+    }
+    else
+    {
+      setState(() {
+        risposta="Errore: "+response.body;
+      });
+    }
+  }
+  Future<bool> endBlink() async
+  {
+    final url = Uri.http("192.168.1.120:8889","/endblink");
+    final response = await http.get(url);
+    if (response.statusCode == 200)
+    {
+      setState(() {
+        risposta=response.body;
+      });
+    }
+    else
+    {
+      setState(() {
+        risposta="Errore: "+response.body;
+      });
+    }
+  }
+}
