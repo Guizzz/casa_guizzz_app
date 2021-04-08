ccc5fb03961faaad5433aa421b3782f3e3422810
Guizzz <aguizzaro98@hotmail.it>
2021-04-08 18:37:54 +0200

    add pwm led controller

---
+++ lib/HomePage.dart
@@ -0,0 +1,140 @@
+import 'package:casa_guizzz_app/Decorations.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  String risposta = "";
+  double ledValue = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 3,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    InkWell(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width / 3,
+                        height: MediaQuery.of(context).size.height / 8,
+                        decoration: BoxDecoration(
+                            borderRadius: new BorderRadius.circular(10.0),
+                            color: Colors.white,
+                            boxShadow: [Decorations.shadow()]),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: FittedBox(
+                              child: Text(
+                            "Blink",
+                            style: new TextStyle(color: Colors.black),
+                          )),
+                        ),
+                      ),
+                      onTap: blink,
+                    ),
+                    InkWell(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width / 3,
+                        height: MediaQuery.of(context).size.height / 8,
+                        decoration: BoxDecoration(
+                            borderRadius: new BorderRadius.circular(10.0),
+                            color: Colors.white,
+                            boxShadow: [Decorations.shadow()]),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: FittedBox(
+                              child: Text(
+                            "Stop",
+                            style: new TextStyle(color: Colors.black),
+                          )),
+                        ),
+                      ),
+                      onTap: endBlink,
+                    ),
+                  ],
+                ),
+              ),
+              Slider(
+                  value: ledValue,
+                  min: 0,
+                  max: 255,
+                  label: ledValue.toInt().toString(),
+                  onChanged: (double value) {
+                    setState(() {
+                      setLED(value.toInt());
+                      ledValue = value;
+                    });
+                  }),
+              Text(risposta),
+            ],
+          ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future<bool> setLED(int val) async {
+    Map<String, String> parameters = {'ledVal': val.toString()};
+    final url = Uri.http("192.168.1.120:8889", "/setLED", parameters);
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      setState(() {
+        risposta = response.body;
+      });
+    } else {
+      setState(() {
+        risposta = "Errore: " + response.body;
+      });
+    }
+  }
+
+  Future<bool> blink() async {
+    final url = Uri.http("192.168.1.120:8889", "/blink");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      setState(() {
+        risposta = response.body;
+      });
+    } else {
+      setState(() {
+        risposta = "Errore: " + response.body;
+      });
+    }
+  }
+
+  Future<bool> endBlink() async {
+    final url = Uri.http("192.168.1.120:8889", "/endblink");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      setState(() {
+        risposta = response.body;
+      });
+    } else {
+      setState(() {
+        risposta = "Errore: " + response.body;
+      });
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
+import 'package:casa_guizzz_app/HomePage.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -16,96 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Casa Guizzz App'),
+      home: HomePage(title: 'Casa Guizzz App'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String risposta = "";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 3,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  InkWell(
-                    child: Container(
-                      width: MediaQuery.of(context).size.width / 3,
-                      height: MediaQuery.of(context).size.height / 8,
-                      decoration: BoxDecoration(
-                        borderRadius: new BorderRadius.circular(10.0),
-                        color: Colors.blue,
-                      ),
-                    ),
-                    onTap: blink,
-                  ),
-                  InkWell(
-                    child: Container(
-                      width: MediaQuery.of(context).size.width / 3,
-                      height: MediaQuery.of(context).size.height / 8,
-                      decoration: BoxDecoration(
-                        borderRadius: new BorderRadius.circular(10.0),
-                        color: Colors.red,
-                      ),
-                    ),
-                    onTap: endBlink,
-                  ),
-                ],
-              ),
-            ),
-            Text(risposta),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  Future<bool> blink() async {
-    final url = Uri.http("192.168.1.120:8889", "/blink");
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      setState(() {
-        risposta = response.body;
-      });
-    } else {
-      setState(() {
-        risposta = "Errore: " + response.body;
-      });
-    }
-  }
-
-  Future<bool> endBlink() async {
-    final url = Uri.http("192.168.1.120:8889", "/endblink");
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      setState(() {
-        risposta = response.body;
-      });
-    } else {
-      setState(() {
-        risposta = "Errore: " + response.body;
-      });
-    }
   }
 }
